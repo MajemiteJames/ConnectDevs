8cf16b7f48c2245a0e1bbb67017b2fc2b39d71a9
MajemiteJames <majemiteokoro@gmail.com>
2019-10-14 17:36:57 +0200

    created all the users routes dummy routes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURL');
+
+const connectDB = async () => {
+    try{
+        mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected.....')
+    }catch(err) {
+        console.error(err.message);
+        //Exit the process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
