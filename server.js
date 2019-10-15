662afb4464068cc5b64bf3d583fd34cea188ac12
MajemiteJames <majemiteokoro@gmail.com>
2019-10-15 13:49:58 +0200

    make the user route to send a jwt token on successful registeration

--- server.js
+++ server.js
@@ -6,6 +6,9 @@
   // Connecting Database
   connectDB();
 
+  // Init Middleware
+  app.use(express.json({ extended: false }));
+
   app.get('/',  (req, res) => res.send('API Runing'));
 
   // Define Routes
