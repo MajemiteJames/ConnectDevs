e56ccc5c119645cb32a3413cea27b4c5b39e3d9e
MajemiteJames <majemiteokoro@gmail.com>
2019-10-23 10:00:29 +0200

    implemeted the clearing of storage so a new use can log in with previuos use details deleted and designed the create profile link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,17 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout}) => {
 
     const authLinks = (
         <ul>
-            <li><Link onClick={logout} to="#!"> <i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Logout</span></Link></li>
+            <li>
+                <Link to="/dashboard"> 
+                <i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Dashboard </span>
+                </Link>
+            </li>
+
+            <li>
+                <Link onClick={logout} to="#!"> 
+                <i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Logout</span>
+                </Link>
+            </li>
         </ul>
     );
 
