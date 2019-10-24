044bd951d3dbee2871c46a1123b10db7365b0e9a
MajemiteJames <majemiteokoro@gmail.com>
2019-10-24 11:13:01 +0200

    implented the add experience and add education page with the viewing of education and experience on the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardAction from './DashboardAction'
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -19,6 +21,8 @@ const Dashboard = ({getCurrentProfile, auth: { user }, profile: { profile, loadi
 
         {profile !== null ? <Fragment>
             <DashboardAction/>
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
         </Fragment> : (<Fragment>
             <p>You have not yet setup a profile, please add some info</p>
             <Link to='/create-profile' className="btn btn-primary my-1">
